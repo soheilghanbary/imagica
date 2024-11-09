d253bbd8a1f99d8cb9952edae6ceb19a26faa112
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-09 22:29:16 +0330

    update hero

--- src/components/(app)/hero.tsx
+++ src/components/(app)/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
     <section className="space-y-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:py-20">
       <div className="mx-auto flex max-w-5xl flex-col items-center gap-4 text-center">
         <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl/tight">
-          Imagica a Cool photo searcher from all over the world
+          a Cool photo searcher from all over the world
         </h1>
         <p className="max-w-[42rem] text-muted-foreground leading-normal sm:text-xl sm:leading-8">
           Modern image sharing platform powered by Next.js 15.
