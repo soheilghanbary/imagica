374458077ef7415ac1d31ac264c509e26504b59d
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-10 22:04:42 +0330

    add search icon to search photo input

--- src/components/(app)/search-photo.tsx
+++ src/components/(app)/search-photo.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { SearchIcon } from 'lucide-react';
 import { useQueryState } from 'nuqs';
 import { useDebouncedCallback } from 'use-debounce';
 
@@ -10,12 +11,13 @@ export function SearchPhoto() {
 
   return (
     <div className="relative mx-auto mt-4 mb-6 flex max-w-lg items-center justify-center gap-2">
+      <SearchIcon className="-translate-y-1/2 absolute top-1/2 left-4 size-4 text-muted-foreground" />
       <input
         type="text"
         defaultValue={query}
-        placeholder="search any photos"
+        placeholder="Search any photos"
         onChange={(e) => debounced(e.target.value)}
-        className="h-11 w-full rounded-full border border-border bg-background px-4 py-2 text-foreground/80 text-sm shadow-sm ring-primary duration-150 focus:ring-2"
+        className="h-11 w-full rounded-full border border-border bg-background px-4 py-2 pl-10 text-foreground/80 text-sm placeholder-muted-foreground shadow-sm ring-primary duration-150 focus:ring-2"
       />
     </div>
   );
